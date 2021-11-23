3475f660e5c98359e8e64f51aaf3f1dac593f294
ds-lukaslangr <ds-lukaslangr>
2021-11-23 12:18:41 +0000

    u

--- dbxpoetry.py
+++ dbxpoetry.py
@@ -1,12 +1,12 @@
 # Databricks notebook source
+pip install benvy==1.2.2.dev2
+
+# COMMAND ----------
+
 dbutils.widgets.dropdown("1. poetry action", choices=["add", "update", "remove"], defaultValue="add")
 dbutils.widgets.text("2. package", defaultValue="")
 poetry_action = dbutils.widgets.get("1. poetry action")
 package = dbutils.widgets.get("2. package")
-
-# COMMAND ----------
-
-pip install benvy==1.2.2.dev2
 
 # COMMAND ----------
 
