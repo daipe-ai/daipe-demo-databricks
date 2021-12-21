5f38114f905ca36171ddc35fe9764e3243ff8d2a
kutny <jirkakoutny@gmail.com>
2021-12-21 06:17:30 +0100

    Jobs definition & processing updated

--- tools/dbx_jobs.py
+++ tools/dbx_jobs.py
@@ -4,7 +4,7 @@
 
 # COMMAND ----------
 
-# MAGIC %pip install jobs-bundle=="0.4.0" &> /dev/null
+# MAGIC %pip install jobs-bundle=="0.5.0" &> /dev/null
 
 # COMMAND ----------
 
@@ -32,7 +32,7 @@ def create_or_update_job(command: JobCreateOrUpdateCommand, identifier: str):
     cwd_parts = os.getcwd().split("/")
 
     command.run(
-        Namespace(identifier=identifier, user_folder_name=cwd_parts[3], repo_name=cwd_parts[4],)
+        Namespace(identifier=identifier, user_folder_name=cwd_parts[3], dir_name=cwd_parts[4],)
     )
 
 # COMMAND ----------
@@ -40,7 +40,7 @@ def create_or_update_job(command: JobCreateOrUpdateCommand, identifier: str):
 @notebook_function(get_widget_value("job"), "%jobsbundle.jobs%")
 def create_job(job: str, jobs, command: JobCreateOrUpdateCommand):
     if job == "<all>":
-      for job in jobs:
-        create_or_update_job(command, job)
+        for job in jobs:
+            create_or_update_job(command, job)
     else:
         create_or_update_job(command, job)
